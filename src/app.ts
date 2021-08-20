0f596dc2a496d7ab7f3cf64823d8225042ae50c2
sys970717 <sys970717@naver.com>
2021-08-20 12:19:49 +0900

    initialize

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import express, {Request,Response,Application} from 'express';
+
+const app: express.Application = express();
+
+app.get("/", (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    res.send("hello typescript express!");
+});
+
+export default app;
